d4863a0b138f79e9af45f10afd3e57cf646b31c6
so1icitx <tojitiktok@proton.me>
2025-10-15 16:32:54 +0300

    added an exit, added a better shell prompt, also added comments and fixed some bugs

--- main.c
+++ main.c
@@ -1,45 +1,84 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <string.h>
 
-int main(int argc, char** argv)
+
+int main()
 {
-    while (1)
-    {
-        printf(">> ");
-        fflush(stdout);
-        int f_num = fork(); 
-        if(f_num < 0){
-            return 0;
-        }
-        else if(f_num == 0)
+   while(1)
+   {
+        
+        char* input = calloc(256, sizeof(char));
+        char* args[256];
+        char hostname[64];
+        char username[64];
+        char wd [128];
+        char* pwd;
+        
+        gethostname(hostname, sizeof(hostname));
+        getlogin_r(username, sizeof(username));
+        getcwd(wd, sizeof(wd));
+        if (strcspn(wd, "h") == 1)
         {
-            char* input = calloc(64, 1);
-            if (fgets(input, 64, stdin) == NULL)
-            {
-                printf("Invalid input!\n");
-                exit(1);
-            }
-            fflush(stdin);
-            char* args[64];
-            char* piece = strtok(input, " \n");
-            
-            int i = 0;
-            while (piece != NULL){
-
-                args[i] = piece;
-                i++;
-                piece = strtok(NULL, " \n"); 
-            }
-            args[i] = NULL;
-            execvp(args[0], args); 
+            wd[4] = '~';
+            pwd = &(wd[4]);
         }
-        else{
-            wait(NULL);
+        printf("%s@%s %s> ", username, hostname, pwd);
+        fflush(stdout);
+        
+
+        if (fgets(input, 128, stdin) == NULL)
+        {
+            fprintf(stderr, "error: invalid input\n");
+            continue;
         }
         
-    }
-   
+        input[strcspn(input, "\n")] = 0;
+        if (strlen(input) <= 1)
+        {
+            fprintf(stderr, "error: invalid input\n");
+            continue;
+        }
+
+        if(strcmp(input, "exit") == 0)
+        {
+            free(input);
+            break;
+        }
+        // forking a child proccess + checking its result
+        int fk_num = fork();
+
+        // if fork failed
+        if (fk_num < 0)
+        {
+            fprintf(stderr, "error: fork failed!");
+            free(input);
+            exit(1);
+        }
+
+        // if child forking was successful
+        else if(fk_num == 0)
+        {
+            char* token = strtok(input, " \n");
+            int i = 0;
+            while (token != NULL)
+            {
+                args[i] = token;
+                i++;
+                token = strtok(NULL, " \n");
+            }
+            args[i] = NULL;
+
+            execvp(args[0], args);
+            break;
+        }
+
+        // if parent
+        else
+        {
+            wait(NULL);
+        }
+   }
 }
