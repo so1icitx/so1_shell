145f60a7b71114278ccde2736383ed17ae4d5f70
so1icitx <tojitiktok@proton.me>
2025-10-21 15:44:06 +0300

    added command history (a big pain is my a23023) + had to add termios for up/down arrow key detection

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <linux/limits.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -5,23 +6,101 @@
 #include <sys/wait.h>
 #include <string.h>
 #include <errno.h>
+#include <termios.h>
 
 #define SIZEOFINPUT 128
 #define HOSTNUSRNAME 32
+#define HISTORYCOUNT 12
+static char history[HISTORYCOUNT][SIZEOFINPUT];
+static int history_count = 0;
+static int history_ptr = 0;
 
+// checks if function was succesful
 void isValid(int(*f)(char*, size_t), char* argument1)
 {
-   if( f(argument1, HOSTNUSRNAME) != 0)
+   if(f(argument1, HOSTNUSRNAME) != 0)
    {
        fprintf(stderr, "error: %s\n", strerror(errno));
    }
 }
+
+// sets up or removes char by char input
+void setUpTerminal(int operation)
+{
+    struct termios old_settings, new_settings;
+    tcgetattr(STDIN_FILENO, &old_settings);
+    new_settings = old_settings;
+    if (operation == 1)
+    {
+        tcsetattr(STDIN_FILENO, TCSAFLUSH, &old_settings);
+    }
+    else{
+        new_settings.c_lflag &= ~(ICANON | ECHO);
+        tcsetattr(STDIN_FILENO, TCSAFLUSH, &new_settings);
+    } 
+}
+
+// appending history logic
+void addHistory(char* args)
+{
+
+    if (history_count < HISTORYCOUNT - 1)
+    {
+        strcpy(history[history_count], args);
+        history_count++;
+        history_ptr = history_count;
+    }
+    else
+    {
+        for(int i = 0; i < history_count; i++)
+        {
+            strcpy(history[i], history[i + 1]);
+        }
+        strcpy(history[history_count], args);
+        history_ptr = history_count;
+    }
+}
+
+// querying history logic
+int getHistory(char direction, char* input, int* pos) {
+    int new_pos = *pos;
+    int i;
+
+    if (direction == 'u' && history_ptr > 0) {
+        // erase current line
+        for (i = 0; i < new_pos; i++)
+            write(STDOUT_FILENO, "\b \b", 3);
+
+        history_ptr--;
+        strcpy(input, history[history_ptr]);
+        new_pos = strlen(history[history_ptr]);
+        write(STDOUT_FILENO, input, new_pos);
+        *pos = new_pos;
+        return new_pos;
+    }
+
+    else if (direction == 'd' && history_ptr < history_count - 1) {
+        // erase current line
+        for (i = 0; i < new_pos; i++)
+            write(STDOUT_FILENO, "\b \b", 3);
+
+        history_ptr++;
+        strcpy(input, history[history_ptr]);
+        new_pos = strlen(history[history_ptr]);
+        write(STDOUT_FILENO, input, new_pos);
+        *pos = new_pos;
+        return new_pos;
+    }
+
+    return *pos; 
+}
+
 int main()
 {
    while(1)
    {
         char* input = calloc(SIZEOFINPUT, sizeof(char));
-        char* args[SIZEOFINPUT];
+        char* args[SIZEOFINPUT]; 
         char hostname[HOSTNUSRNAME];
         char username[HOSTNUSRNAME];
         char wd [SIZEOFINPUT];
@@ -30,12 +109,12 @@ int main()
         //validating
         isValid(&gethostname, hostname);
         isValid(&getlogin_r, username);
- 
         if (getcwd(wd, sizeof(wd)) == NULL)
         {
              fprintf(stderr, "error: %s\n", strerror(errno));
         }
 
+        // home shell check
         if (strcspn(wd, "h") == 1)
         {
             wd[4 + strlen(username) + 1] = '~';
@@ -50,27 +129,95 @@ int main()
         printf("%s@%s %s> ", username, hostname, pwd);
         fflush(stdout);
         
-        // validates input
-        if (fgets(input, SIZEOFINPUT, stdin) == NULL)
-        {
-            fprintf(stderr, "error: %s\n", strerror(errno));
-            continue;
+        // setups up line by line char interpreting
+        setUpTerminal(0);
+        int pos = 0;
+        char c;
+        int esc_state = 0;
+        while (1)
+        { 
+            read(STDIN_FILENO, &c, sizeof(char));
+
+            if (esc_state == 0)
+            {
+                // detects esc char (\033)
+                if(c == 27)
+                {
+                    esc_state = 1;
+                }
+                
+                // detects enter keybind (\n) on some terminals enter gets detected as \r because of ICANON
+                else if (c == '\n' || c == '\r')
+                {
+                    input[pos] = '\0';
+                    write(STDOUT_FILENO, "\n", sizeof(char));
+                    break;
+                }
+                // detects backspace
+                else if(c == 127)
+                {
+                    if(pos > 0)
+                    {
+                        pos--;
+                        input[pos] = '\0';
+                        write(STDOUT_FILENO, "\b \b", 3);
+                    }
+                }
+                else
+                {
+                    input[pos] = c;
+                    pos++;
+                    write(STDOUT_FILENO, &c, sizeof(char));
+                }
+
+            }
+            // detects [ char
+            else if (esc_state == 1)
+            {
+                
+                if (c == 91)
+                {
+                    esc_state = 2;
+                }
+            }
+            else if (esc_state == 2)
+            {
+                esc_state = 0;
+                // detects up arrow (A)
+                if (c == 65)
+                {
+                    pos = getHistory('u', input, &pos);
+                }
+                // detects down arrow (B)
+                else if (c == 66)
+                {
+                    pos = getHistory('d', input, &pos);
+                }
+            }
         }
+        setUpTerminal(1);
         fflush(stdin);
+        fflush(stdout);
+
+        // validates input
         if (strlen(input) <= 1)
         {
             fprintf(stderr, "error: invalid input\n");
             continue;
         }
-        // removes newline char because its annoying
-        input[strcspn(input, "\n")] = 0;
-        
-        
+         
+        //exits the shell
+        if(strcmp(input, "exit") == 0)
+        {
+            free(input);
+            break;
+        }
+
         // cd command logic
         if (input[0] == 'c' && input [1] == 'd')
         {
             
-            if (input[3] == '\0')
+            if (input[2] == '\0')
             {
                 char cd_home[HOSTNUSRNAME];
                 snprintf(cd_home, HOSTNUSRNAME, "/home/%s", username);
@@ -91,12 +238,7 @@ int main()
             continue;
         }
 
-        //exits the shell
-        if(strcmp(input, "exit") == 0)
-        {
-            free(input);
-            break;
-        }
+        
 
         // forking the parent process
         int f_num = fork();
@@ -110,11 +252,12 @@ int main()
         }
 
         // child process route 
-        else if(f_num == 0)
+        else if (f_num == 0)
         {
             // parses input
             char* token = strtok(input, " \n");
             int i = 0;
+            printf("%s", token);
             while (token != NULL)
             {
                 args[i] = token;
@@ -122,7 +265,7 @@ int main()
                 token = strtok(NULL, " \n");
             }
             args[i] = NULL;
-
+            
             if (execvp(args[0], args) == -1){
                 free(input);
                 fprintf(stderr, "error: %s\n", strerror(errno));
@@ -134,6 +277,7 @@ int main()
         else
         {   
             wait(NULL);
+            addHistory(input);
             free(input);
         }
    }
